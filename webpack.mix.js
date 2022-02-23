d86a54b9a1b7475a22188471f942094bfe50ea76
root <root@localhost.dev>
2022-02-23 18:33:19 +0200

    downgrade to v8

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,17 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
+// mix for default theme
+mix.copyDirectory('themes/default/assets/img', 'public/themes/default/img');
+mix.copyDirectory('themes/default/assets/fonts', 'public/themes/default/fonts');
+// js
+mix.js(['themes/default/assets/js/app.js'], 'public/themes/default/js/app.min.js')
+//sass
+mix.sass('themes/default/assets/sass/app.scss', 'public/themes/default/css/app.min.css')
+// mix for admin theme
+mix.copyDirectory('themes/default/assets/img', 'public/themes/default/img');
+mix.copyDirectory('themes/default/assets/fonts', 'public/themes/default/fonts');
+// js
+mix.js(['themes/default/assets/js/app.js'], 'public/themes/default/js/app.min.js')
+//sass
+mix.sass('themes/default/assets/sass/app.scss', 'public/themes/default/css/app.min.css')
