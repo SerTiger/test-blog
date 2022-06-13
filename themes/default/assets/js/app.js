a99a00c41c941288880055345cf1bccb157a1713
“GrafValdemar” <erevanskiyshpek@gmail.com>
2022-06-13 20:30:53 +0300

    executed cleanup & done sidebar

--- themes/default/assets/js/app.js
+++ themes/default/assets/js/app.js
@@ -1,7 +1,4 @@
-require('./contact')
-require('./invest_first')
-require('./inquire_first')
-require('./inquire_second')
+
 $(window).on("load", () => {
     setTimeout(function () {
         $('.loader').fadeOut('fast');
@@ -10,31 +7,5 @@ $(window).on("load", () => {
 });
 
 $(document).ready(() => {
-    AOS.init({
-        disable: function () {
-            const maxWidth = 700;
-            return window.innerWidth <= maxWidth;
-        }
-    });
-    window.addEventListener('scroll',()=>{
-        window.scrollY > 130 ? $('.header').addClass('fixed') : $('.header').removeClass('fixed')
-    })
-    window.scrollY > 130 ? $('.header').addClass('fixed') : ''
-    $('.scroll-down').on('click', function (){
-        window.scrollTo({
-            top: 700,
-            behavior: "smooth"
-        });
-    })
-    $('.select').select2({
-        minimumResultsForSearch: -1
-    })
-    $('.burger').on('click', function (){
-        $('.burger-menu').addClass('active')
-        $('body').addClass('fixed-body')
-    })
-    $('.close-burger').on('click', function (){
-        $('.burger-menu').removeClass('active')
-        $('body').removeClass('fixed-body')
-    })
+
 })
